bbbaf245286e8c13e0f2d3d8025fec82aa62527d
vinicius2 <viniciussantosfrota10012017@gmail.com>
2023-12-10 08:01:15 -0300

    Hooks
    criação dos hooks

---
+++ src/hooks/useFetchDocuments.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect, useCallback } from "react";
+import { db } from "../firebase/Config";
+import { collection, query, where, orderBy, getDocs } from "firebase/firestore";
+
+export const useFetchDocuments = (docCollection, search = null, uid = null) => {
+    const [documents, setDocuments] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(null);
+
+    // deal with memory leak
+    const [cancelled, setCancelled] = useState(false)
+
+    useEffect(()=> {
+        const getUsers = async () => {
+            if(cancelled) return
+
+            setLoading(true)
+
+            const userCollectionRef = collection(db, docCollection)
+
+            try{
+                let q;
+                if(search){
+                    q = await query(userCollectionRef, where ("tagArray", "array-contains", search), orderBy("createdAt", "desc"))
+                }else if(uid){
+                    q = await query(userCollectionRef, where("uid", "==", uid), orderBy("createdAt", "desc"))
+                }else{
+
+                }
+                q = await getDocs(userCollectionRef);
+                setDocuments(q.docs.map((doc)=>({
+                    ...doc.data(),
+                    id: doc.id
+                })))
+            }catch(error){
+                console.log(error.message)
+                setError(error)
+                setLoading(false)      
+            }
+        }
+        getUsers()
+    },[docCollection, search, uid, cancelled])
+
+    return { documents, loading, error}
+}
---
+++ src/hooks/useInsertDocument.jsx
@@ -0,0 +1,65 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/firestore'
+import { useState, useEffect, useReducer } from 'react'
+import { db } from '../firebase/Config'
+import { collection, addDoc, Timestamp, setDoc } from 'firebase/firestore'
+
+const initialState = {
+    loading: null,
+    error:null
+}
+
+const insertReducer = (state, action) =>{
+    switch(action.type){
+        case "LOADING":
+            return{loading:true, error:null}
+        case "INSERTED_DOC":
+            return{loading:false, error:null}
+        case "ERROR":
+            return{ loading:false, error: action.payload}
+        default:
+            return state
+    }
+}
+
+export const useInsertDocument = (docCollection) => {
+    const [response, dispatch] = useReducer(insertReducer, initialState)
+
+    const [cancelled, setCancelled] = useState(false)
+
+    const checkCancelBeforeDispatch = (action) => {
+        if(!cancelled){
+            dispatch(action)
+        }
+    }
+
+    const insertDocument = async (document) => {
+        checkCancelBeforeDispatch({type: "LOADING"})
+
+        try{
+            const newDocument = {...document, createdAt: Timestamp.now()}
+
+            const insertedDocument = await addDoc(
+                collection(db, docCollection),
+                newDocument
+            )
+
+            checkCancelBeforeDispatch({
+                type: "INSERTED_DOC",
+                payload: insertedDocument,
+            })
+        }catch(error){
+            checkCancelBeforeDispatch({
+                type: "ERROR",
+                payload: error.message
+            })
+        }
+    }
+
+    useEffect(()=>{
+        return () => setCancelled(true)
+
+    }, [])
+
+    return { insertDocument, response}
+}
